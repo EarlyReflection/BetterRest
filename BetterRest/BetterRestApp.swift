d1830d6bc0cc156637285d7739625023b3ca83cd
Vladimir Dvornikov <earlyreflections@protonmail.com>
2023-08-12 16:02:23 +0300

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Vladimir Dvornikov on 12/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
